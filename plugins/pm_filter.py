5709b70870b5ab22d34278db7cc119262a709ea0
Oᴘ Eʟᴇᴄᴛʀᴏ ⚡ <97022591+godhridhikesh@users.noreply.github.com>
2022-06-25 15:27:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,11 +406,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('🍻 ᴀᴅᴅ ᴍᴇ ᴀɴᴅ ɴᴊᴏʏ 🍻 ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
              ],[
-             InlineKeyboardButton('🏝 ᴍʏ ɢʀᴘ 🏝 ', url='https://t.me/mal_rock'),
-             InlineKeyboardButton('🏝 ᴜᴘᴅᴀᴛᴇs 🏝', url='https://t.me/Electro_Botz')
+             InlineKeyboardButton('☘ ᴍʏ ɢʀᴘ ☘ ', url='https://t.me/mal_rock'),
+             InlineKeyboardButton('☘ ᴜᴘᴅᴀᴛᴇs ☘', url='https://t.me/Electro_Botz')
              ],[      
-             InlineKeyboardButton('🏝 ʜᴇʟᴘ 🏝', callback_data='help'),
-             InlineKeyboardButton('🏝 ᴀʙᴏᴜᴛ 🏝', callback_data='about')
+             InlineKeyboardButton('☘ ʜᴇʟᴘ ☘', callback_data='help'),
+             InlineKeyboardButton('☘ ᴀʙᴏᴜᴛ ☘', callback_data='about')
              ],[
              InlineKeyboardButton('🍁 sᴜʙsᴄʀɪʙᴇ 🍁  ', url='https://youtube.com/channel/UC8cwZPS77sTgoX9QD1NLwWw')
         ]]
